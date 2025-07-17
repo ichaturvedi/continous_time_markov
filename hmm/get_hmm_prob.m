63102e24051c774a769df4a8bd877bfd85378824
ichaturvedi <65399216+ichaturvedi@users.noreply.github.com>
2025-07-17 21:23:54 +1000

    Add files via upload

---
+++ hmm/get_hmm_prob.m
@@ -0,0 +1,25 @@
+clear
+
+watch = load("weka_input_apple.csv");
+
+output_train = watch(:,1);
+thr = output_train;
+
+output_train = watch(:,2);
+ang = output_train;
+
+output_train = watch(:,3);
+brk = output_train;
+
+gear = watch(:,4);
+
+thr2 = discretize(thr,3);
+brk2 = discretize(brk,3);
+ang2 = discretize(ang,3);
+gear2 = discretize(gear,3);
+input2 = discretize([normalize(thr,'range') normalize(ang,'range') normalize(brk,'range')],3);
+
+[estimateTR1,estimateE1] = hmmestimate([brk2],gear2);
+[estimateTR2,estimateE2] = hmmestimate(input2,gear2);
+
+save prob.mat estimateE2 estimateTR2;
